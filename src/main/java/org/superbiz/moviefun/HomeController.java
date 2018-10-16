41695f9a9739cb1e2ca3a6f0a808f7ec1e83f4bf
Noguchi Hiromi <hiromi.noguchi@example.com>
2018-10-16 12:27:14 +0900

    Implements multiple data sources for albums and movies

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,6 +1,9 @@
 package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionOperations;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.superbiz.moviefun.albums.Album;
 import org.superbiz.moviefun.albums.AlbumFixtures;
@@ -18,12 +21,23 @@ public class HomeController {
     private final AlbumsBean albumsBean;
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
+    private final PlatformTransactionManager albumsTransactionManager;
+    private final PlatformTransactionManager moviesTransactionManager;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    public HomeController(
+            MoviesBean moviesBean,
+            AlbumsBean albumsBean,
+            MovieFixtures movieFixtures,
+            AlbumFixtures albumFixtures,
+            PlatformTransactionManager albumsTransactionManager,
+            PlatformTransactionManager moviesTransactionManager
+    ) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
         this.albumFixtures = albumFixtures;
+        this.albumsTransactionManager = albumsTransactionManager;
+        this.moviesTransactionManager = moviesTransactionManager;
     }
 
     @GetMapping("/")
@@ -33,13 +47,21 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
-        for (Movie movie : movieFixtures.load()) {
-            moviesBean.addMovie(movie);
-        }
+        new TransactionTemplate(moviesTransactionManager).execute(transactionStatus -> {
+            for (Movie movie : movieFixtures.load()) {
+                moviesBean.addMovie(movie);
+            }
 
-        for (Album album : albumFixtures.load()) {
-            albumsBean.addAlbum(album);
-        }
+            return null;
+        });
+
+        new TransactionTemplate(albumsTransactionManager).execute(transactionStatus -> {
+            for (Album album : albumFixtures.load()) {
+                albumsBean.addAlbum(album);
+            }
+
+            return null;
+        });
 
         model.put("movies", moviesBean.getMovies());
         model.put("albums", albumsBean.getAlbums());
